f4f798322b317d6a5e5a0eafed4b80c5a32242a8
Vivek Pattanaik <vivekpatanaik100@gmail.com>
2018-11-05 22:21:32 +0530

    Data to Reveiw

---
+++ review-data.py
@@ -0,0 +1,93 @@
+review_data = {
+'Marlon Brando': {
+'The Godfather': 5.00, 
+'The Godfather Part II': 4.29,
+'Apocalypse Now': 5.00, 
+'Jaws': 1.
+},
+'Stephen King': {
+'The Shawshank Redemption': 4.89, 
+'The Shining': 4.93 , 
+'The Green Mile': 4.87,
+'The Godfather': 1.33,
+},
+'Steven Spielberg': {
+'Raiders of the Lost Ark': 5.0, 
+'Jaws': 4.89,
+'Saving Private Ryan': 4.78, 
+'Star Wars Episode IV - A New Hope': 4.33,
+'Close Encounters of the Third Kind': 4.77,
+'The Godfather':  1.25,
+'The Godfather Part II': 1.72
+},
+'George Lucas':{
+'Star Wars Episode IV - A New Hope': 5.00	
+},
+'Al Pacino': {
+'The Godfather': 4.02, 
+'The Godfather Part II': 5.00,
+},
+'Robert DeNiro': {
+'The Godfather': 3.07, 
+'The Godfather Part II': 4.29, 
+'Raging Bull': 5.00, 
+'Goodfellas':  4.89
+},
+'Robert Duvall': {
+'The Godfather': 3.80, 
+'The Godfather Part II': 3.61,
+'Apocalypse Now': 4.26 
+},
+'Jack Nicholson': {
+'The Shining': 5.0,
+'One Flew Over The Cuckoos Nest': 5.0,
+'The Godfather': 2.22,
+'The Godfather Part II': 3.34
+},
+'Morgan Freeman': {
+'The Shawshank Redemption': 4.98,
+'The Shining': 4.42,
+'Apocalypse Now': 1.63,
+'The Godfather': 1.12,
+'The Godfather Part II': 2.16
+},
+'Harrison Ford': {
+'Raiders of the Lost Ark': 5.0, 
+'Star Wars Episode IV - A New Hope': 4.84,
+},
+'Tom Hanks': {
+'Saving Private Ryan': 3.78, 
+'The Green Mile': 4.96,
+'The Godfather': 1.04,
+'The Godfather Part II': 1.03
+},
+'Francis Ford Coppola': {
+'The Godfather': 5.00, 
+'The Godfather Part II': 5.0, 
+'Jaws': 1.24,
+'One Flew Over The Cuckoos Nest': 2.02
+},
+'Martin Scorsese': {
+'Raging Bull': 5.0, 
+'Goodfellas': 4.87,
+'Close Encounters of the Third Kind': 1.14,
+'The Godfather': 4.00
+},
+'Diane Keaton': {
+'The Godfather': 2.98,
+'The Godfather Part II': 3.93,
+'Close Encounters of the Third Kind': 1.37
+},
+'Richard Dreyfuss': {
+'Jaws': 5.0, 
+'Close Encounters of the Third Kind': 5.0,
+'The Godfather': 1.07,
+'The Godfather Part II': 0.63
+},
+'Joe Pesci': {
+'Raging Bull': 4.89, 
+'Goodfellas': 5.0,
+'The Godfather': 4.87,
+'Star Wars Episode IV - A New Hope': 1.32	
+}
+}
